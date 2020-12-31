22336642a82c90aaebb25466484faf00f4137b60
James Vickers <jamesvickers19@gmail.com>
2020-12-31 11:59:37 -0700

    allow CORS requests

--- youtube_dl_mixtape/__init__.py
+++ youtube_dl_mixtape/__init__.py
@@ -19,7 +19,9 @@ def create_app():
         if video_id is None:
             return Response("Missing required 'v' parameter (Youtube video id)", status=400)
         tracks = track_labels(f"https://youtube.com/watch?v={video_id}")
-        return Response(json.dumps(tracks), mimetype="application/json")
+        response = Response(json.dumps(tracks), mimetype="application/json")
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        return response
 
     # takes video_id and sections, returns zip (?) of mp4 files
     def download():
